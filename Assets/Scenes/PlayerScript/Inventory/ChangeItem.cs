335f999a0129e4ae832fcb7585976e519cac843c
danil2283376 <67260052+danil2283376@users.noreply.github.com>
2021-03-30 21:07:32 +0300

    remove trash code blocks
    remove trash code blocks

--- Assets/Scenes/PlayerScript/Inventory/ChangeItem.cs
+++ Assets/Scenes/PlayerScript/Inventory/ChangeItem.cs
@@ -12,15 +12,10 @@ public class ChangeItem : MonoBehaviour
         GameObject oldItem = null;
         Transform oldTransform = null;
         Transform child = null;
-        //Transform[] transformChilds = new Transform[gameObject.transform.childCount];
 
         // нахожу старую вещь на игроке
-        //transformChilds = gameObject.GetComponentsInChildren<Transform>();
-        //transformChild = gameObject.GetComponentsInChildren<Transform>();
         for (int i = 0; i < gameObject.transform.childCount; i++) 
         {
-            //print(transformChilds[i].tag);
-            //print(tagItem);
             child = gameObject.transform.GetChild(i);
             if (child.tag == tagItem)
             {
@@ -32,26 +27,19 @@ public class ChangeItem : MonoBehaviour
         }
         // уничтожение объекта старого итема
         Destroy(oldItem);
-        //oldItem.transform.localPosition = new Vector3(oldTransform.transform.position.x, oldTransform.transform.position.y, oldTransform.transform.position.z);
         // поиск в weapons по имени нужного итема
         int indexItem = 0;
         while (indexItem < items.Length) 
         {
             if (items[indexItem].name == nameItem)
-            {
                 break;
-            }
             indexItem++;
         }
         //получил индекс элемента можно использовать для прикрепления к игроку!!!
         // и присобачивание его уже как ребенка к игроку
         //ИНДЕКС НЕ ВЕРНЫЙ ПОДУМАТЬ ПОЧЕМУ
-        //print(indexItem);
-        //items[indexItem].transform.parent = gameObject.transform;
         GameObject clone = Instantiate(items[indexItem], gameObject.transform);
         clone.transform.position = new Vector3(oldTransform.transform.position.x,
             oldTransform.transform.position.y, oldTransform.transform.position.z);
-        //items[indexItem].transform.localPosition = new Vector3(oldTransform.transform.position.x, 
-        //    oldTransform.transform.position.y, oldTransform.transform.position.z);
     }
 }
