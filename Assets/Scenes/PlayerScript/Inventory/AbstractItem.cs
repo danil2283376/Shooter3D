055b2421e03458e1b26332644feeae4e860a8b24
danil2283376 <67260052+danil2283376@users.noreply.github.com>
2021-04-02 07:38:18 +0300

    add normal TextMeshPro
    add normal TextMeshPro

---
+++ Assets/Scenes/PlayerScript/Inventory/AbstractItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public abstract class AbstractItem : MonoBehaviour
+{
+    //public Image 
+}
